e2457796e35092a88c830d88167ee0b597a9a9b4
Tobias Maerz <t.maerz@flowconcept.de>
2020-01-03 09:03:57 +0100

    Initial changes.

--- provision.sh
+++ provision.sh
@@ -1,1 +1,12 @@
-Provision
+#!/usr/bin/env bash
+
+composer install
+npm -s install
+bower install
+gulp build
+
+
+lando drush --yes sql-drop -y
+lando drush --yes sql-sync @lando.staging @lando.dev
+lando drush --yes rsync @lando.staging:%files @lando.dev:%files
+
