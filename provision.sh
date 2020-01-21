caa551ebbdc69c55e8ff1412a5ca0e27b91c3e9b
Tobias Maerz <t.maerz@flowconcept.de>
2020-01-21 11:51:18 +0100

    Added options.

--- provision.sh
+++ provision.sh
@@ -1,12 +1,52 @@
 #!/usr/bin/env bash
 
-composer install
-npm -s install
-bower install
-gulp build
+PS3='Von welchem System sollen die Daten geholt werden: '
+options=("Staging (nur Datenabgleich)" "Staging (komplett)" "Produktion (nur Datenabgleich)" "Produktion (komplett)" "Quit")
 
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Staging (nur Datenabgleich)")
+            INSTALL=0
+            TYPE=staging
+            break            
+            ;;
+        "Staging (komplett)")
+            INSTALL=1
+            TYPE=staging
+            break            
+            ;;
+        "Produktion (nur Datenabgleich)")
+            INSTALL=0
+            TYPE=prod
+            break            
+            ;;
+        "Produktion (komplett)")
+            INSTALL=1
+            TYPE=prod
+            break            
+            ;;
+        "Quit")
+            QUIT=1;
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+
+if [ "$QUIT" = 1 ]; then
+  exit
+fi;
+
+if [ "$INSTALL" = 1 ]; then
+  composer install
+  npm -s install
+  bower install
+  gulp build
+fi
 
 lando drush --yes sql-drop -y
-lando drush --yes sql-sync @lando.staging @lando.dev
-lando drush --yes rsync @lando.staging:%files @lando.dev:%files
+lando drush --yes sql-sync @lando.$TYPE @lando.dev
+lando drush --yes rsync @lando.$TYPE:%files @lando.dev:%files
 
